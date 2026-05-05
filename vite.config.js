16caba1e6ca011e4a91dc602725359c4a64e7840
Priyanshu Joshi <priyanshujoshi2252.com>
2026-05-05 12:17:36 +0530

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are loaded with relative paths, fixing GitHub Pages issues
+  base: '/Portfolio/', // Set to the GitHub repository name
 })
